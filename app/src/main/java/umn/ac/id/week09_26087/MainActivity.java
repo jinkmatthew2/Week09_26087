40267dc73dc2e8a089b98566021bbfa936ad8396
Michael <michaelrony17@gmail.com>
2020-04-13 21:14:45 +0700

    Third Commit

--- app/src/main/java/umn/ac/id/week09_26087/MainActivity.java
+++ app/src/main/java/umn/ac/id/week09_26087/MainActivity.java
@@ -125,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        /// WELL
+        /// There is Something WOng
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             mhsVM.deleteAll();
